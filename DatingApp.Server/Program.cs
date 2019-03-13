00f6f98bf546fca8ae171455a38a32dcd8ffd6e1
Алекс Курбатов <akourbat@gmail.com>
2019-03-13 13:58:24 +0300

    Add project files.

---
+++ DatingApp.Server/Program.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Hosting;
+
+namespace DatingApp.Server
+{
+    public class Program
+    {
+        [Obsolete]
+        public static async Task Main(string[] args)
+        {
+            var silo = new SiloHostBuilder()
+                   .UseLocalhostClustering()
+                   //.ConfigureServices(services =>
+                   //{
+                   //    services.AddDbContext<UserDbSet>(o => o.UseSqlite("Data Source=UserTest.db"));
+                   //    services.AddMediatR(typeof(Startup).Assembly);
+                   //})
+                   .EnableDirectClient()
+                   .Build();
+
+            await silo.StartAsync();
+
+            var client = silo.Services.GetRequiredService<IClusterClient>();
+
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        services.AddSingleton<IGrainFactory>(client);
+                        services.AddSingleton<IClusterClient>(client);
+                    });
+                    webBuilder.UseStartup<Startup>();
+                })
+                .Build().RunAsync();
+        }
+    }
+
+    public interface IMyGrain : IGrainWithIntegerKey
+    {
+        Task<T> Echo<T>(T value);
+    }
+
+    public class MyGrain : Grain, IMyGrain
+    {
+        public Task<T> Echo<T>(T value) => Task.FromResult(value);
+    }
+}
---
+++ DatingApp.Server/ViewModels/GreetingVM.cs
@@ -0,0 +1,67 @@
+﻿using FluentValidation;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Server.ViewModels
+{
+    public class ReactiveFodyViewModel : ReactiveObject
+    {
+        public ReactiveCommand<Unit, Unit> Clear { get; }
+
+        [ObservableAsProperty]
+        public string Greeting { get; }
+
+        [ObservableAsProperty]
+        public IEnumerable<string> Results { get; }
+
+        [ObservableAsProperty]
+        public bool HasErrors { get; }
+
+        [ObservableAsProperty]
+        public bool CanExecute { get; }
+
+        [Reactive]
+        public string Name { get; set; } = string.Empty;
+
+        private NameValidator Validator { get; }
+
+        public ReactiveFodyViewModel()
+        {
+            this.WhenAnyValue(x => x.Name, (name) => !string.IsNullOrEmpty(name) && name.Length >= 4 )
+                .ToPropertyEx(this, x => x.CanExecute);
+
+            Clear = ReactiveCommand.Create(() => { this.Name = string.Empty; }, this.WhenAnyValue(x => x.CanExecute));
+
+            this.WhenAnyValue(x => x.Name)
+                .Select(name => $"Hello, {name}!")
+                .ToPropertyEx(this, x => x.Greeting);
+
+            this.Validator = new NameValidator();
+
+            this.WhenAnyValue(x => x.Name)
+                .Throttle(TimeSpan.FromMilliseconds(800))
+                .DistinctUntilChanged()
+                .Where(name => !string.IsNullOrWhiteSpace(name))
+                .Select(name => Validator.Validate(this).Errors.Select(e => e.ErrorMessage))
+                .ToPropertyEx(this, x => x.Results);
+
+            this.WhenAnyValue(x => x.Results, l => l != null && l.Any())
+                .ToPropertyEx(this, x => x.HasErrors);
+        }
+    }
+
+    public class NameValidator : AbstractValidator<ReactiveFodyViewModel>
+    {
+        public NameValidator()
+        {
+            RuleFor(n => n.Name).MaximumLength(8).WithMessage("Hey it is too long, dude!");
+        }
+    }
+}
